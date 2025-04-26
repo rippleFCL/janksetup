7e2d55a15302ebea4a3ecf601df56f8b21b63070
ripple <github@ripplefcl.com>
2025-04-26 16:37:00 +0100

    feat(rofi): switched theme

--- ansible/files/dotfiles/.config/rofi/scripts/jumper.sh
+++ ansible/files/dotfiles/.config/rofi/scripts/jumper.sh
@@ -11,10 +11,12 @@
 ## style-6     style-7     style-8     style-9     style-10
 ## style-11    style-12    style-13    style-14    style-15
 
-dir="$HOME/.config/rofi/launchers/type-2"
-theme='style-1'
+dir="$HOME/.config/rofi/launchers/type-1"
+theme='style-3'
 
 ## Run
 rofi \
-    -show drun \
-    -theme ${dir}/${theme}.rasi
+    -show window \
+    -theme ${dir}/${theme}.rasi \
+    -dpi 1
+
